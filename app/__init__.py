83c6505cfbdcf3ca6e7f050863a4573d33826a81
Atiksh Jha <atikshjha2007@gmail.com>
2025-04-01 00:26:06 -0400

    temp secert key

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = "2338724829301376494238947647623886783284y9"  # Set the secret key here
     app.config['DEBUG'] = True
 
     from app.db import init_db
